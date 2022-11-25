b37b618f1941190237ed8da347bc1005b524f052
Madhav-07 <79003708+Madhav-07@users.noreply.github.com>
2022-11-25 10:33:23 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from program import summation
+
+class TestSummation(unittest.Testcase):
+  def test_Sum1(self):
+    result1 = summation(2)
+    self.assertEqual(result1,3)
+  
+  def test_Sum2(self):
+    result2 = summation(0)
+    self.assertEqual(result2,0)
+    
+  def test_Sum3(self):
+    result3 = summation(5)
+    self.assertEqual(result3,15)
+    
+if __name__ == "__main__":
+  unittest.main()
